b4c6a028321395821a4af973353e64b8858f7ec1
kwyszczelski <wyszczelskikonrad@gmail.com>
2022-07-28 03:57:22 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -6,88 +6,90 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-            String url="jdbc:mysql://localhost:3306/magazyn";
-            String username="root";
-            String password="";
+        String url = "jdbc:mysql://localhost:3306/magazyn";
+        String username = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
 
             Scanner input = new Scanner(System.in);
+            // REGISTER
+
             // imie
             System.out.println("Podaj swoje imię");
             String imie = input.nextLine();
             // Nazwisko
             System.out.println("Podaj swoje nazwisko");
             String nazwisko = input.nextLine();
-            // Data urodzenia
-            System.out.println("Podaj swoją datę urodzin");
-            String dat_uro = input.nextLine();
-            // Nr tel
-            System.out.println("Podaj nr tel");
-            String nr_tel = input.nextLine();
-            // Dzial
-            System.out.println("Podaj dział w którym pracujesz");
-            String dzial = input.nextLine();
+            // email
+            System.out.println("Podaj adres e-mail");
+            String email = input.nextLine();
 
-            System.out.println( "Imie: "+ imie + " Nazwisko: " + nazwisko+ " Data Urodzenia:  "+ dat_uro  + " Nr Telefonu: " + nr_tel+ " Dział: " + dzial);
-            System.out.println("Czy podane przez ciebie dane są prawidłowe?: (Tak/Nie) ");
-            String check = input.nextLine();
+            //czy email jest już w bazie
 
-            //if czy sie zgadza
-        if (check.equals("Tak"))
-        {
-            try
+            ResultSet resultSet = statement.executeQuery("select * from employees where email='"+ email +"'");
+
+            if (resultSet.next()==false)
             {
-                Class.forName("com.mysql.cj.jdbc.Driver");
 
-                Connection connection = DriverManager.getConnection(url, username, password);
-                Statement statement = connection.createStatement();
-
-                String insertStr = "INSERT INTO  employees VALUES (?, ?,?,?,?,?)";
-                try (PreparedStatement pst = connection.prepareStatement(insertStr))
+                if(email.contains("@") && email.contains("."))
                 {
-                    pst.setString(1, null);
-                    pst.setString(2,imie);
-                    pst.setString(3,nazwisko);
-                    pst.setString(4,null);
-                    pst.setString(5,nr_tel);
-                    pst.setString(6,dzial);
-                    pst.executeUpdate();
+                    // Data urodzenia
+                    System.out.println("Podaj swoją datę urodzenia (R-M-D)");
+                    String dat_uro = input.nextLine();
+                    // Nr tel
+                    System.out.println("Podaj nr tel");
+                    String nr_tel = input.nextLine();
+                    // Dzial
+                    System.out.println("Podaj dział w którym pracujesz");
+                    String dzial = input.nextLine();
+
+                    System.out.println( "Imie: "+ imie + " Nazwisko: " + nazwisko+ " Data Urodzenia:  "+ dat_uro  + " Nr Telefonu: " + nr_tel+ " Dział: " + dzial);
+                    System.out.println("Czy podane przez ciebie dane są prawidłowe?: (Tak/Nie) ");
+                    String check = input.nextLine();
+
+                    if (check.equals("Tak"))
+                    {
+                        String insertStr = "INSERT INTO  employees VALUES (?,?,?,?,?,?,?)";
+                        try (PreparedStatement pst = connection.prepareStatement(insertStr))
+                        {
+                            pst.setString(1, null);
+                            pst.setString(2,imie);
+                            pst.setString(3,nazwisko);
+                            pst.setString(4,email);
+                            pst.setString(5,dat_uro);
+                            pst.setString(6,nr_tel);
+                            pst.setString(7,dzial);
+                            pst.executeUpdate();
+                            System.out.println("Zostałeś zarejestrowany!");
+                        }
+                        catch (Exception e)
+                        {
+                            System.out.println("Wystąpił problem podczas dodawania danych do bazy.");
+                        }
+                    }
+                    else // Odpowiedz uzytkownika nieprawilowe dane
+                    {
+                        System.out.println("Spróbuj ponownie.");
+                    }
                 }
-                catch (Exception e)
+                else // Adres email nie zawiera (@, .)
                 {
-                    System.out.println("Error");
+                    System.out.println("Podany adres, nie jest adresem email. Spróbuj ponownie");
                 }
-
-
-
-                //statement.executeUpdate("Insert INTO employees VALUES (null, imie, nazwisko, null, nr_tel, dzial )");
-
-                /*System.out.println("Wszystkie dane z bazy: ");
-                ResultSet resultSet=statement.executeQuery("select * from employees");
-                while(resultSet.next())
-                {
-                    System.out.println(resultSet.getInt(1) + " "+ resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getDate(4) + " " + resultSet.getString(5) + " " + resultSet.getString(6));
-                }
-
-                 */
-
-
-                connection.close();
-
             }
-            catch(Exception e)
+            else //Email odrzucony - jest już w bazie
             {
-                System.out.println(e);
+                System.out.println("Podany email został już wykorzystany.");
             }
         }
-        else if (check.equals("Nie"))
-        {
-            System.out.println("Spróbuj jeszcze raz");
+        catch(Exception e ){
+            System.out.println("Wystąpił problem w połączeniu z bazą danych." +e);
         }
-        else
-        {
-            System.out.println("Komenda nie została rozpoznana, spróbuj ponownie");
-        }
-
 
     }
 }
